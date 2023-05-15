7ff35573bdc28fa18fd2aff859dc482bd68ae93f
fa <zuchairul@gmail.com>
2023-05-15 20:50:29 +0800

    admin auth

--- src/api/admin/admin.routes.ts
+++ src/api/admin/admin.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import AuthRoutes from './auth/auth.routes';
+
 const router = Router();
 
-
+router.use('/auth', AuthRoutes);
 
 export default router;
--- src/api/api.routes..ts
+++ src/api/api.routes..ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserRoutes from './user/user.routes'
+import AdminRoutes from './admin/admin.routes'
 
 const router = Router();
 
 router.use('/user', UserRoutes);
+router.use('/admin', AdminRoutes);
 
 export default router;
